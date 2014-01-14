a49c5582f230e614b8e169307e3b3debd820932e
Matt Hinchliffe <matt@maketea.co.uk>
2014-01-14 21:23:57 +0000

    Add .editorconfig and ensure compliance

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,38 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/js',
-            src: ['**/*.js'],
-            dest: 'dist/js'
-          }
-        ]
-      }
-    },
-    jshint: {
-      all: ['src/js/**/*.js'],
-      options: {
-        maxdepth: 4,
-        curly: true,
-        newcap: true,
-        eqeqeq: true,
-        browser: true,
-        trailing: true,
-        globals: {
-          jquery: true,
-          define: false,
-          exports: true
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/js',
+                    src: ['**/*.js'],
+                    dest: 'dist/js'
+                }]
+            }
+        },
+        jshint: {
+            all: ['src/js/**/*.js'],
+            options: {
+                maxdepth: 4,
+                curly: true,
+                newcap: true,
+                eqeqeq: true,
+                browser: true,
+                trailing: true,
+                globals: {
+                    jquery: true,
+                    define: false,
+                    exports: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
